732724869c3e836d0c5e5422bc8ec24e254cb9a4
Wilco de Boer <deboer.wilco@gmail.com>
2026-07-13 11:22:16 +0200

    Fix recipe creation typing

--- scripts/globals/Crafting.gd
+++ scripts/globals/Crafting.gd
@@ -13,7 +13,7 @@ func _ready() -> void:
 	recipes.append(recipe)
 
 	recipe.display_name = "Smelt Metal"
-	recipe.inputs = {Stockpile.ItemType.ORE: 1}
-	recipe.outputs = {Stockpile.ItemType.INGOT: 1}
+	recipe.inputs[Stockpile.ItemType.ORE] = 1
+	recipe.outputs[Stockpile.ItemType.INGOT] = 1
 	recipe.work = 5.0
-	recipe.needs_capabilities = [Capabilities.FURNACE]
+	recipe.needs_capabilities.append(Capabilities.FURNACE)

767c81705e5303b5e3c2e77d93863c3ffd303b4b
Wilco de Boer <deboer.wilco@gmail.com>
2026-07-13 17:12:55 +0200

    Make known resource tracking global

--- scripts/ResourceList.gd
+++ scripts/ResourceList.gd
@@ -2,7 +2,6 @@ extends VBoxContainer
 
 
 var _labels: Dictionary[Stockpile.ItemType, Label] = {}
-var _seen: Dictionary[Stockpile.ItemType, bool] = {}
 
 
 func _ready() -> void:
@@ -10,21 +9,14 @@ func _ready() -> void:
 		var label := Label.new()
 		add_child(label)
 		_labels[item] = label
-		_seen[item] = false
+		label.hide()
+
 	Stockpile.changed.connect(_refresh)
 	_refresh()
 
 
 func _refresh() -> void:
 	for item in Stockpile.ItemTypes:
-		var amount = Stockpile.get_amount(item)
-
-		if amount > 0:
-			_seen[item] = true
-
-		if _seen[item]:
+		if Stockpile.is_seen(item):
 			_labels[item].text = "%s: %d" % [Stockpile.get_display_name(item), Stockpile.get_amount(item)]
 			_labels[item].show()
-
-		else:
-			_labels[item].hide()
--- scripts/globals/Stockpile.gd
+++ scripts/globals/Stockpile.gd
@@ -22,6 +22,7 @@ const _ITEM_NAMES: Dictionary[ItemType, String] = {
 }
 
 var _amounts: Dictionary[ItemType, int] = {}
+var _seen: Dictionary[ItemType, bool] = {}
 
 
 func _ready() -> void:
@@ -29,27 +30,32 @@ func _ready() -> void:
 		if item != ItemType.NONE:
 			ItemTypes.append(item)
 			_amounts[item] = 0
+			_seen[item] = false
 
 
 func add(item: ItemType, amount: int) -> void:
 	_amounts[item] += amount
+	_seen[item] = true
 	changed.emit()
 
 
 func add_bulk(items: Dictionary[ItemType, int]) -> void:
 	for item in items:
 		_amounts[item] += items[item]
+		_seen[item] = true
 	changed.emit()
 
 
 func remove(item: ItemType, amount: int) -> void:
 	_amounts[item] -= amount
+	_seen[item] = true
 	changed.emit()
 
 
 func remove_bulk(items: Dictionary[ItemType, int]) -> void:
 	for item in items:
 		_amounts[item] -= items[item]
+		_seen[item] = true
 	changed.emit()
 
 
@@ -61,3 +67,7 @@ func get_display_name(item: ItemType) -> String:
 	if not _ITEM_NAMES.has(item):
 		return "???"
 	return _ITEM_NAMES[item]
+
+
+func is_seen(item: ItemType) -> bool:
+	return _seen[item]

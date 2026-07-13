2effde65f21acc9d2cbe21946358c1b6c43f482a
Wilco de Boer <deboer.wilco@gmail.com>
2026-07-13 07:18:43 +0200

    Add empty Workshop UI

--- objects/buildings/Workshop.gd
+++ objects/buildings/Workshop.gd
@@ -1,9 +1,15 @@
 extends Building
 
 
+const POPUP := preload("res://objects/buildings/WorkshopPopup.tscn")
+
 var capabilities: Array[Crafting.Capabilities] = [Crafting.Capabilities.FURNACE]
 var order: Recipe = null
 
 
 func get_display_name() -> String:
 	return "Workshop"
+
+
+func get_popup() -> PackedScene:
+	return POPUP

159be6185580ddc30ac35761af7914c922bba330
Wilco de Boer <deboer.wilco@gmail.com>
2026-07-14 23:49:22 +0200

    Add map panning with MMB

--- scripts/Viewport.gd
+++ scripts/Viewport.gd
@@ -6,6 +6,8 @@ extends Camera2D
 @export var min_zoom: float = 0.5
 @export var max_zoom: float = 2
 
+var _panning := false
+
 
 func _ready() -> void:
 	zoom = Vector2(min_zoom, min_zoom)
@@ -18,6 +20,9 @@ func _process(delta: float) -> void:
 	)
 	position += input * pan_speed * delta / sqrt(zoom.x)
 
+	if _panning and not Input.is_mouse_button_pressed(MOUSE_BUTTON_MIDDLE):
+		_panning = false
+
 
 func _unhandled_input(event: InputEvent) -> void:
 	if event is InputEventMouseButton and event.pressed:
@@ -25,11 +30,18 @@ func _unhandled_input(event: InputEvent) -> void:
 			_zoom_by(1 + zoom_factor)
 		elif event.button_index == MOUSE_BUTTON_WHEEL_DOWN:
 			_zoom_by(1 / (1 + zoom_factor))
+		elif event.button_index == MOUSE_BUTTON_MIDDLE:
+			_panning = true
 	elif event is InputEventKey and event.pressed:
 		if event.keycode == KEY_EQUAL or event.keycode == KEY_KP_ADD:
 			_zoom_by(1 + zoom_factor)
 		elif event.keycode == KEY_MINUS or event.keycode == KEY_KP_SUBTRACT:
 			_zoom_by(1 / (1 + zoom_factor))
+
+
+func _input(event: InputEvent) -> void:
+	if _panning and event is InputEventMouseMotion:
+		position -= event.relative / zoom
 
 
 func _zoom_by(delta: float) -> void:

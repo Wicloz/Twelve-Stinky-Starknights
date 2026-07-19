42d7e29443d37608e8007a8fdca2c201e4352062
Wilco de Boer <deboer.wilco@gmail.com>
2026-07-19 17:03:16 +0200

    Fix null bug for the workshop sprite

--- scripts/Building.gd
+++ scripts/Building.gd
@@ -9,7 +9,13 @@ signal constructed
 		_fit_sprite_to_tile()
 
 const HOLO_BLUE := preload("res://assets/shaders/holo_blue.tres")
-@onready var _sprite := $Sprite2D
+
+var _sprite_holder: Sprite2D = null
+var _sprite: Sprite2D:
+	get:
+		if _sprite_holder == null:
+			_sprite_holder = $Sprite2D
+		return _sprite_holder
 
 var _under_construction: bool = false
 var _refund: Dictionary[Stockpile.ItemType, int] = {}

8b3057238c138e50a2bc9ad66d4f873e594db18c
Wilco de Boer <deboer.wilco@gmail.com>
2026-09-06 09:14:39 +0200

    Add a song that's also valid AWA5.0

--- scripts/MusicPlayer.gd
+++ scripts/MusicPlayer.gd
@@ -88,10 +88,11 @@ func _define_playlists() -> void:
 	_add("AI Generated", preload("res://assets/music/suno/Stinky Starknights.ogg"))
 	_add("AI Generated", preload("res://assets/music/suno/Eating for Twelve.ogg"))
 	_add("AI Generated", preload("res://assets/music/suno/Wrong Star, Baby.ogg"))
-	_add("AI Generated", preload("res://assets/music/suno/Starknights build tonight.ogg"))
-	_add("AI Generated", preload("res://assets/music/suno/Phase Connect.ogg"))
-	_add("AI Generated", preload("res://assets/music/suno/Titanium.ogg"))
 	_add("AI Generated", preload("res://assets/music/suno/Grinding on the Hex.ogg"))
+	_add("AI Generated", preload("res://assets/music/suno/Phase Connect.ogg"))
+	_add("AI Generated", preload("res://assets/music/suno/Starknights build tonight.ogg"))
+	_add("AI Generated", preload("res://assets/music/suno/Titanium.ogg"))
+	_add("AI Generated", preload("res://assets/music/suno/Be Water.ogg"))
 	_add("AI Generated", preload("res://assets/music/suno/We are the Starknights.ogg"))
 
 

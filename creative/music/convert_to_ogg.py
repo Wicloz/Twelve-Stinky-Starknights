25b621ad4edb3a1f56b1d243218bd865f7ec1f55
Wilco de Boer <deboer.wilco@gmail.com>
2026-09-09 17:03:33 +0200

    Redownload and reconvert all current songs

--- creative/music/convert_to_ogg.py
+++ creative/music/convert_to_ogg.py
@@ -3,7 +3,6 @@ from pathlib import Path
 
 
 if __name__ == "__main__":
-    for wav_file in Path("creative/music").glob("*.wav"):
+    for wav_file in sorted(Path("creative/music").glob("*.wav")):
         ogg_file = wav_file.with_suffix(".ogg")
-        if not ogg_file.exists():
-            run(["ffmpeg", "-i", str(wav_file), "-c:a", "libvorbis", "-q:a", "7", str(ogg_file)])
+        run(["ffmpeg", "-i", str(wav_file),"-c:a", "libvorbis", "-q:a", "7","-bitexact",str(ogg_file)])
